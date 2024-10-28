ef901130ff74ac3bdc113db5b96fdac72a67241a
Arun <terraform1008@outlook.com>
2024-10-28 07:20:26 +0000

    backend added

--- provider.tf
+++ provider.tf
@@ -9,6 +9,14 @@ terraform {
 }
 
 
+backend "azurerm" {
+    resource_group_name  = "terrastore"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "terrastore1008"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "tfstate"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "prod.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
+
+
 # The resource block defines the type of resource we want to work with
 # The name and location are arguements for the resource block
 
