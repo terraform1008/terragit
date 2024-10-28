0579cdc8aaab9c01a3733ef54dff861f47252506
Arun <terraform1008@outlook.com>
2024-10-28 07:00:06 +0000

    my first change

---
+++ provider.tf
@@ -0,0 +1,18 @@
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+
+# The resource block defines the type of resource we want to work with
+# The name and location are arguements for the resource block
+
+resource "azurerm_resource_group" "app_grp"{
+  name="app-grp" 
+  location="North Europe"
+}
